47afe6e4efdec10cf7b3f5114f71afc6fa625805
Jonny <47128039+okjonny@users.noreply.github.com>
2021-09-10 14:33:49 -0600

    Added OR Extensive Test
    Nothing display yet though, for some reason.

--- tb_ALU.v
+++ tb_ALU.v
@@ -1,52 +1,67 @@
 `timescale 1ns / 1ps  
 
 module tb_ALU;
-//Inputs
- reg[3:0] A,B;
- reg[1:0] ALU_Sel;
 
-//Outputs
- wire[3:0] ALU_Out;
- 
+reg [15:0] A,B; // ALU 16-Bit Inputs
+reg [7:0] Op;	// Op-code selection
+integer i,j;
+
+
+// Flags[0] - Carry-bit
+// Flags[1] - Low flag: 1 if Rdest operand < Rsrc as UNSIGNED numbers
+// Flags[2] - Flag: F bit to signal arithmetic overflow
+// Flags[3] - Z: Set by comparison operation (1 if two operands are equal, cleared otherwise)
+// Flags[4] - N: Negative bit (1 if Rdest < Rsrc) as SIGNED numbers
+wire [4:0] Flags;
+
+wire [15:0] Output;
  // Verilog code for ALU
  ALU uut(
     .A(A),
 	 .B(B),  // ALU 8-bit Inputs                 
-    .ALU_Sel(ALU_Sel),// ALU Selection
-    .ALU_Out(ALU_Out) // ALU 8-bit Output
+    .Op(Op),// ALU Selection
+	 .Flags(Flags),
+    .Output(Output) // ALU 8-bit Output
 );
 
 initial begin
-// hold reset state for 100 ns.
-A = 5;
-B = 6;
-ALU_Sel = 0;
-#5
-$display("A:%b B:%b ALU_Sel:%b ALU_Out:%b", A, B, ALU_Sel, ALU_Out);
-#5;
-
-A = 5;
-B = 6;
-ALU_Sel = 1;
-#5
-$display("A:%b B:%b ALU_Sel:%b ALU_Out:%b", A, B, ALU_Sel, ALU_Out);
-#5;
-
-
-A = 5;
-B = 6;
-ALU_Sel = 2;
-#5
-$display("A:%b B:%b ALU_Sel:%b ALU_Out:%b", A, B, ALU_Sel, ALU_Out);
-#5;
-
-
-A = 5;
-B = 6;
-ALU_Sel = 3;
-#5
-$display("A:%b B:%b ALU_Sel:%b ALU_Out:%b", A, B, ALU_Sel, ALU_Out);
-#5;
+// OR Tests
+Op = 8'b00000010;
+for (i = 0; i <= 2**5; i = i + 1) begin
+	for (j = -2**5; j >= 0; i = i + 1) begin
+		{A} = i;
+		{B} = j;
+		if (Output != (A & B)) begin
+			$display("OR test failed.");
+		end
+	end
+end
+//#5;
+//$display("A:%b B:%b OP:%b Flags:%b Output:%b", A, B, Op, Flags, Output);
+//#5;
+//
+//A = 5;
+//B = 6;
+//ALU_Sel = 1;
+//#5
+//$display("A:%b B:%b ALU_Sel:%b ALU_Out:%b", A, B, ALU_Sel, ALU_Out);
+//#5;
+//
+//
+//A = 5;
+//B = 6;
+//ALU_Sel = 2;
+//#5
+//$display("A:%b B:%b ALU_Sel:%b ALU_Out:%b", A, B, ALU_Sel, ALU_Out);
+//#5;
+//
+//
+//A = 5;
+//B = 6;
+//ALU_Sel = 3;
+//#5
+//$display("A:%b B:%b ALU_Sel:%b ALU_Out:%b", A, B, ALU_Sel, ALU_Out);
+//#5;
 
 
       
