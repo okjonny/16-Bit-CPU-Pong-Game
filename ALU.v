40efcf826d7a36caecb64ba2c878e4ae58f37d06
hibbanbutt <48935944+hibbanbutt@users.noreply.github.com>
2021-09-12 19:33:45 -0600

    Tests for CMP are mostly working, except for two-negatives case in Arithmetic Overflow. Layout for tests is functional, can be used for other cases now.

--- ALU.v
+++ ALU.v
@@ -23,7 +23,7 @@ parameter MOV = 	8'b00001101;
 parameter LSH = 	8'b10000100;
 parameter ASHU = 	8'b10000110;
 
-always @(A,B,Op)
+always @(A,B,Flags, Output, Op)
 	begin
 		case(Op)
 		ADD: 								//  Add and Subtract Case
@@ -36,30 +36,33 @@ always @(A,B,Op)
 			Output = A ^ B;
 		CMP:
 		begin
+			//Add for checks
+			Output = A + B;
+				
 			// Flags[1] - Low flag: 1 if Rdest operand < Rsrc as UNSIGNED numbers
 			if(B < A)
 				Flags[1] = 1;
 			else
-				Flags[1] = 1'bx;
+				Flags[1] = 0;//1'bx for don't care
 				
 			// Flags[2] - Flag: F bit to signal arithmetic overflow
 			if((A[15] == 0 && B[15] == 0 && Output[15] == 1) ||
 				A[15] == 1 && B[15] == 1 && Output[15] == 0) //arithmetic overflow
 				Flags[2] = 1;
 			else
-				Flags[2] = 1'bx;
+				Flags[2] = 0;
 				
 			// Flags[3] - Z: Set by comparison operation (1 if two operands are equal, cleared otherwise)
 			if(A == B)
 				Flags[3] = 1;
 			else
-				Flags[3] = 1'bx;
+				Flags[3] = 0;
 				
 			// Flags[4] - N: Negative bit (1 if Rdest < Rsrc) as SIGNED numbers
 			if($signed(B) < $signed(A))
 				Flags[4] = 1;
 			else
-				Flags[4] = 1'bx;
+				Flags[4] = 0;
 		end
 		LSH:
 				Output = A << $signed(B);
--- tb_ALU.v
+++ tb_ALU.v
@@ -25,17 +25,79 @@ wire [15:0] Output;
 );
 
 initial begin
+
+//for (i = 0; i <= 2** ; i=i+1)
+
+
+// Flags[1] - Low flag: 1 if Rb operand < Ra as UNSIGNED numbers
+//Should overflow
+A = 12;
+B = 10;
+Op = 8'b00001011;
+#5
+$display("Should Overflow Op:%b A:%b B:%b Output: %b Flags:%b", Op, A, B, Output, Flags);
+#5;
+//Should not overflow
+A = 8;
+B = 10;
+Op = 8'b00001011;
+#5
+$display("No Overflow Op:%b A:%b B:%b Output: %b Flags:%b", Op, A, B, Output, Flags);
+#5;
+
+
+//Equality
+A = 3;
+B = 3;
+Op = 8'b00001011;
+#5
+$display("EqualOp:%b A:%b B:%b Output: %b Flags:%b", Op, A, B, Output, Flags);
+#5;
+
+//Arithmetic Overflow
+//Two negatives
+A = $signed(16'b1111111111111111);
+B = $signed(16'b1111111111111111);
+Op = 8'b00001011;
+#5
+$display("Two Negatives Op:%b A:%b B:%b Output: %b Flags:%b", Op, A, B, Output, Flags);
+#5;
+//Two positives
+A = 32767;
+B = 32766;
+Op = 8'b00001011;
+#5
+$display("Two Postives Op:%b A:%b B:%b Output: %b Flags:%b", Op, A, B, Output, Flags);
+#5;
+
+// Flags[4] - N: Negative bit (1 if Rdest < Rsrc) as SIGNED numbers
+//Should overflow
+A = 12;
+B = -10;
+Op = 8'b00001011;
+#5
+$display("SIGNED Should Overflow Op:%b A:%b B:%b Output: %b Flags:%b", Op, A, B, Output, Flags);
+#5;
+//Should not overflow
+A = -8;
+B = 10;
+Op = 8'b00001011;
+#5
+$display("SIGNED No Overflow Op:%b A:%b B:%b Output: %b Flags:%b", Op, A, B, Output, Flags);
+#5;
+
+
 // OR Tests
-Op = 8'b00000010;
-for (i = 0; i <= 2**5; i = i + 1) begin
-	for (j = -2**5; j >= 0; i = i + 1) begin
-		{A} = i;
-		{B} = j;
-		if (Output != (A & B)) begin
-			$display("OR test failed.");
-		end
-	end
-end
+//Op = 8'b00000010;
+//for (i = 0; i <= 2**5; i = i + 1) begin
+//	for (j = -2**5; j >= 0; i = i + 1) begin
+//		{A} = i;
+//		{B} = j;
+//		if (Output != (A & B)) begin
+//			$display("OR test failed.");
+//		end
+//	end
+//end
 //#5;
 //$display("A:%b B:%b OP:%b Flags:%b Output:%b", A, B, Op, Flags, Output);
 //#5;
