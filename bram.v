90b0732c77a0b4e9275f74f1f2f3e2c9f693a2a4
Carlos Gonzalez <77692383+Greenking1014@users.noreply.github.com>
2021-10-05 15:19:14 -0600

    opened up the bram code the professor gave to us. Now we need to test it to make sure it works.

---
+++ bram.v
@@ -0,0 +1,50 @@
+// Quartus Prime Verilog Template
+// True Dual Port RAM with single clock
+
+module bram
+#(parameter DATA_WIDTH=48, parameter ADDR_WIDTH=10)
+(
+	input [(DATA_WIDTH-1):0] data_a, data_b,
+	input [(ADDR_WIDTH-1):0] addr_a, addr_b,
+	input we_a, we_b, clk,
+	output reg [(DATA_WIDTH-1):0] q_a, q_b
+);
+
+	// Declare the RAM variable
+	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+	integer i;
+	initial
+	begin
+		for(i=0;i<1024;i=i+1)
+			ram[i] = i[15:0]; 
+	end
+
+	// Port A 
+	always @ (posedge clk)
+	begin
+		if (we_a) 
+		begin
+			ram[addr_a] <= data_a;
+			q_a <= data_a;
+		end
+		else 
+		begin
+			q_a <= ram[addr_a];
+		end 
+	end 
+
+	// Port B 
+	always @ (posedge clk)
+	begin
+		if (we_b) 
+		begin
+			ram[addr_b] <= data_b;
+			q_b <= data_b;
+		end
+		else 
+		begin
+			q_b <= ram[addr_b];
+		end 
+	end
+
+endmodule
