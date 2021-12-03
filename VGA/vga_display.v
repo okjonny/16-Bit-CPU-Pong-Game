cc811f42b7ba0ef90beb823b6dce84ba966f0d9b
zyad-youssef <zyadibrahimyoussef@gmail.com>
2021-12-02 19:14:32 -0700

    WOMP WOMP
    VGA BROKE

--- VGA/vga_display.v
+++ VGA/vga_display.v
@@ -31,6 +31,10 @@ parameter V_TOTAL       = 10'd525; // total width -- 2 + 33 + 480 + 10 = 525
 assign hsync = ~((hcount >= H_FRONT_PORCH) & (hcount < H_FRONT_PORCH + H_SYNC));
 assign vsync = ~((vcount >= V_DISPLAY_INT + V_FRONT_PORCH) & (vcount < V_DISPLAY_INT + V_FRONT_PORCH + V_SYNC));
 
+assign r = 4'b1111;
+assign g = 4'b1111;
+assign b = 4'b1111;
+
 always @(posedge clk) begin
 	
 	if (rst) begin
@@ -41,15 +45,15 @@ always @(posedge clk) begin
 	end
 	
 	else if (count) begin
-		hcount <= hcount + 1'b1;
+		hcount = hcount + 1'b1;
 		
 		// clear if reaches end
 		if (hcount == H_TOTAL) begin
-			hcount <= 10'd0;
-			vcount <= vcount + 1'b1;
+			hcount = 10'd0;
+			vcount = vcount + 1'b1;
 			
 			if (vcount == V_TOTAL)
-				vcount <= 10'd0;
+				vcount = 10'd0;
 		end
 	end
 	
@@ -70,192 +74,192 @@ always @(hcount,vcount) begin
 		
 end
 
-// THIS IS NOT MY CODE!!!!! USESD FOR TESTING PURPROSES
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// pattern generate
-		always @ (posedge clk)
-		begin
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 1
-			if (vcount < 135)
-				begin              
-					r_red <= 4'hF;    // white
-					r_blue <= 4'hF;
-					r_green <= 4'hF;
-				end  // if (vcount < 135)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 1
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 2
-			else if (vcount >= 135 && vcount < 205)
-				begin 
-					if (hcount < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (hcount < 324)
-					else if (hcount >= 324 && hcount < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 324 && hcount < 604)
-					else if (hcount >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 604)
-					end  // else if (vcount >= 135 && vcount < 205)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 2
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 3
-			else if (vcount >= 205 && vcount < 217)
-				begin 
-					if (hcount < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (hcount < 324)
-					else if (hcount >= 324 && hcount < 371)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 324 && hcount < 371)
-					else if (hcount >= 371 && hcount < 383)
-						begin 
-							r_red <= 4'h0;    // black
-							r_blue <= 4'h0;
-							r_green <= 4'h0;
-						end  // else if (hcount >= 371 && hcount < 383)
-					else if (hcount >= 383 && hcount < 545)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 383 && hcount < 545)
-					else if (hcount >= 545 && hcount < 557)
-						begin 
-							r_red <= 4'h0;    // black
-							r_blue <= 4'h0;
-							r_green <= 4'h0;
-						end  // else if (hcount >= 545 && hcount < 557)
-					else if (hcount >= 557 && hcount < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 557 && hcount < 604)
-					else if (hcount >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 604)
-				end  // else if (vcount >= 205 && vcount < 217)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 3
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 4
-			else if (vcount >= 217 && vcount < 305)
-				begin
-					if (hcount < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (hcount < 324)
-					else if (hcount >= 324 && hcount < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 324 && hcount < 604)
-					else if (hcount >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 604)	
-				end  // else if (vcount >= 217 && vcount < 305)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 4
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 5
-			else if (vcount >= 305 && vcount < 310)
-				begin
-					if (hcount < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (hcount < 324)
-					else if (hcount >= 324 && hcount < 371)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 324 && hcount < 371)
-					else if (hcount >= 371 && hcount < 557)
-						begin 
-							r_red <= 4'h0;    // black
-							r_blue <= 4'h0;
-							r_green <= 4'h0;
-						end  // else if (hcount >= 371 && hcount < 557)
-					else if (hcount >= 557 && hcount < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 557 && hcount < 604)
-					else if (hcount >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 604)	
-				end  // else if (vcount >= 217 && vcount < 305)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 5
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 6
-			else if (vcount >= 305 && vcount < 414)
-				begin
-					if (hcount < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (hcount < 324)
-					else if (hcount >= 324 && hcount < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 324 && hcount < 604)
-					else if (hcount >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (hcount >= 604)	
-				end  // else if (vcount >= 305 && vcount < 414)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 6
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 7
-			else if (vcount <= 414)
-				begin              
-					r_red <= 4'hF;    // white
-					r_blue <= 4'hF;
-					r_green <= 4'hF;
-				end  // if (vcount >= 414)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 7
-		end  // always
-						
+//// THIS IS NOT MY CODE!!!!! USESD FOR TESTING PURPROSES
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//	// pattern generate
+//		always @ (posedge clk)
+//		begin
+//			////////////////////////////////////////////////////////////////////////////////////// SECTION 1
+//			if (vcount < 135)
+//				begin              
+//					r_red <= 4'hF;    // white
+//					r_blue <= 4'hF;
+//					r_green <= 4'hF;
+//				end  // if (vcount < 135)
+//			////////////////////////////////////////////////////////////////////////////////////// END SECTION 1
+//			
+//			////////////////////////////////////////////////////////////////////////////////////// SECTION 2
+//			else if (vcount >= 135 && vcount < 205)
+//				begin 
+//					if (hcount < 324)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // if (hcount < 324)
+//					else if (hcount >= 324 && hcount < 604)
+//						begin 
+//							r_red <= 4'hF;    // yellow
+//							r_blue <= 4'h0;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 324 && hcount < 604)
+//					else if (hcount >= 604)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 604)
+//					end  // else if (vcount >= 135 && vcount < 205)
+//			////////////////////////////////////////////////////////////////////////////////////// END SECTION 2
+//			
+//			////////////////////////////////////////////////////////////////////////////////////// SECTION 3
+//			else if (vcount >= 205 && vcount < 217)
+//				begin 
+//					if (hcount < 324)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // if (hcount < 324)
+//					else if (hcount >= 324 && hcount < 371)
+//						begin 
+//							r_red <= 4'hF;    // yellow
+//							r_blue <= 4'h0;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 324 && hcount < 371)
+//					else if (hcount >= 371 && hcount < 383)
+//						begin 
+//							r_red <= 4'h0;    // black
+//							r_blue <= 4'h0;
+//							r_green <= 4'h0;
+//						end  // else if (hcount >= 371 && hcount < 383)
+//					else if (hcount >= 383 && hcount < 545)
+//						begin 
+//							r_red <= 4'hF;    // yellow
+//							r_blue <= 4'h0;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 383 && hcount < 545)
+//					else if (hcount >= 545 && hcount < 557)
+//						begin 
+//							r_red <= 4'h0;    // black
+//							r_blue <= 4'h0;
+//							r_green <= 4'h0;
+//						end  // else if (hcount >= 545 && hcount < 557)
+//					else if (hcount >= 557 && hcount < 604)
+//						begin 
+//							r_red <= 4'hF;    // yellow
+//							r_blue <= 4'h0;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 557 && hcount < 604)
+//					else if (hcount >= 604)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 604)
+//				end  // else if (vcount >= 205 && vcount < 217)
+//			////////////////////////////////////////////////////////////////////////////////////// END SECTION 3
+//			
+//			////////////////////////////////////////////////////////////////////////////////////// SECTION 4
+//			else if (vcount >= 217 && vcount < 305)
+//				begin
+//					if (hcount < 324)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // if (hcount < 324)
+//					else if (hcount >= 324 && hcount < 604)
+//						begin 
+//							r_red <= 4'hF;    // yellow
+//							r_blue <= 4'h0;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 324 && hcount < 604)
+//					else if (hcount >= 604)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 604)	
+//				end  // else if (vcount >= 217 && vcount < 305)
+//			////////////////////////////////////////////////////////////////////////////////////// END SECTION 4
+//			
+//			////////////////////////////////////////////////////////////////////////////////////// SECTION 5
+//			else if (vcount >= 305 && vcount < 310)
+//				begin
+//					if (hcount < 324)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // if (hcount < 324)
+//					else if (hcount >= 324 && hcount < 371)
+//						begin 
+//							r_red <= 4'hF;    // yellow
+//							r_blue <= 4'h0;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 324 && hcount < 371)
+//					else if (hcount >= 371 && hcount < 557)
+//						begin 
+//							r_red <= 4'h0;    // black
+//							r_blue <= 4'h0;
+//							r_green <= 4'h0;
+//						end  // else if (hcount >= 371 && hcount < 557)
+//					else if (hcount >= 557 && hcount < 604)
+//						begin 
+//							r_red <= 4'hF;    // yellow
+//							r_blue <= 4'h0;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 557 && hcount < 604)
+//					else if (hcount >= 604)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 604)	
+//				end  // else if (vcount >= 217 && vcount < 305)
+//			////////////////////////////////////////////////////////////////////////////////////// END SECTION 5
+//			
+//			////////////////////////////////////////////////////////////////////////////////////// SECTION 6
+//			else if (vcount >= 305 && vcount < 414)
+//				begin
+//					if (hcount < 324)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // if (hcount < 324)
+//					else if (hcount >= 324 && hcount < 604)
+//						begin 
+//							r_red <= 4'hF;    // yellow
+//							r_blue <= 4'h0;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 324 && hcount < 604)
+//					else if (hcount >= 604)
+//						begin 
+//							r_red <= 4'hF;    // white
+//							r_blue <= 4'hF;
+//							r_green <= 4'hF;
+//						end  // else if (hcount >= 604)	
+//				end  // else if (vcount >= 305 && vcount < 414)
+//			////////////////////////////////////////////////////////////////////////////////////// END SECTION 6
+//			
+//			////////////////////////////////////////////////////////////////////////////////////// SECTION 7
+//			else if (vcount <= 414)
+//				begin              
+//					r_red <= 4'hF;    // white
+//					r_blue <= 4'hF;
+//					r_green <= 4'hF;
+//				end  // if (vcount >= 414)
+//			////////////////////////////////////////////////////////////////////////////////////// END SECTION 7
+//		end  // always
+//						
 	// end pattern generate
 
 // Update RGB Values 
-assign r = ((hcount > H_SYNC + H_BACK_PORCH + H_FRONT_PORCH)  && (vcount > V_SYNC + V_BACK_PORCH + V_FRONT_PORCH)) ? r_red : 4'h0;
-assign g = ((hcount > H_SYNC + H_BACK_PORCH + H_FRONT_PORCH)  && (vcount > V_SYNC + V_BACK_PORCH + V_FRONT_PORCH)) ? r_green : 4'h0;
-assign b = ((hcount > H_SYNC + H_BACK_PORCH + H_FRONT_PORCH)  && (vcount > V_SYNC + V_BACK_PORCH + V_FRONT_PORCH)) ? r_blue: 4'h0;
+//assign r = ((hcount > H_SYNC + H_BACK_PORCH + H_FRONT_PORCH)  && (vcount > V_SYNC + V_BACK_PORCH + V_FRONT_PORCH)) ? r_red : 4'h0;
+//assign g = ((hcount > H_SYNC + H_BACK_PORCH + H_FRONT_PORCH)  && (vcount > V_SYNC + V_BACK_PORCH + V_FRONT_PORCH)) ? r_green : 4'h0;
+//assign b = ((hcount > H_SYNC + H_BACK_PORCH + H_FRONT_PORCH)  && (vcount > V_SYNC + V_BACK_PORCH + V_FRONT_PORCH)) ? r_blue: 4'h0;
 
 
 
