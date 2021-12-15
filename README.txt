fa669041598280372f9ee656f8175f5661ee04d4
Carlos Gonzalez <77692383+Greenking1014@users.noreply.github.com>
2021-12-15 14:25:14 -0700

    Organized all the files and created a README to explain how to compile our project and which files to include in the project.

---
+++ README.txt
@@ -0,0 +1,13 @@
+12/15/2021
+
+This is our 3710 CPU Project created by Carlos, Zyad, Hibban and Jonny.
+
+We have seperated our files into different folders containing all their respective files.
+If you wish to run our final code, you need to add CPU, CPU_Wrapper, CPU_FSM
+vga_control, bram, nes_input, RegBank, Decoder_4to16, instruction_reg, 
+ALU, MUX_16to1, MUX_2to1, hexTo7Seg, Five_Bit_Register, Program_counter, pc_displace,
+decoder and clkDivider.
+
+Set CPU_Wrapper as the top level entity.
+
+Thank you.  
--- VGA/vga_control.v
+++ VGA/vga_control.v
@@ -1,227 +1,128 @@
 module vga_control (
     input clk,
-    output vertical_sync,
-    output horizontal_sync,
-    output reg vga_blank_n,
-    output [3:0] r,
-    output [3:0] g,
-    output [3:0] b
+    input [15:0] mem_in,
+    output reg [7:0] red, green, blue,
+    output hsync, vsync,
+    output reg blankN, 
+    output wire vgaClk
 );
 
-wire clk_div_25;
-wire enable_vertical_count;
-wire [15:0] vertical_count;
-wire [15:0] horizontal_count;
+    localparam H_SYNC                 = 10'd96;  // 3.8us  -- 25.175M * 3.8u  = 95.665
+    localparam H_FRONT_PORCH          = 10'd16;  // 0.6us  -- 25.175M * 0.6u  = 15.105
+    localparam H_DISPLAY_INT          = 10'd640; // 25.4us -- 25.175M * 25.4u = 639.445
+    localparam H_BACK_PORCH           = 10'd48;  // 1.9us  -- 25.175M * 1.9u  = 47.8325
+    localparam H_TOTAL                = 10'd800; // total width -- 96 + 16 + 640 + 48 = 800
 
-reg [3:0] r_red = 4'b0;
-reg [3:0] r_blue = 4'b0;
-reg [3:0] r_green = 4'b0;
+    localparam V_SYNC                 = 10'd2;   // 2 lines
+    localparam V_BACK_PORCH           = 10'd33;  // 33 lines
+    localparam V_DISPLAY_INT          = 10'd480; // 480 lines
+    localparam V_FRONT_PORCH          = 10'd10;  // 10 lines
+    localparam V_TOTAL                = 10'd525; // total width -- 2 + 33 + 480 + 10 = 525
+    
+    reg [15:0] h_range_i, h_range_e;
+    
+    
+    reg [9:0] hcount, vcount;
+    
+    wire [15:0] data1, data2;
+    
+    
 
-clock_divider clock_25_d(.clk(clk), .div_clk(clk_div_25));
-horizontal_vga_counter h_count(.clk_div(clk_div_25), .horizontal_count(horizontal_count), .enable_vertical_count(enable_vertical_count));
-vertical_vga_counter v_count(.clk_div(clk_div_25), .enable_vertical_count(enable_vertical_count), .vertical_count(vertical_count));
+    
+    clkDivider divider (
+        .refclk(clk),
+        .rst(0),
+        .outclk_0(vgaClk)
+    );
+    
+    
+    initial begin
+        red = 8'b0;
+        green = 8'b0;
+        blue = 8'b0;
+        blankN = 1'b0;
+        //vgaClk = 1'b0;
+        hcount = 10'b0;
+        vcount = 10'b0;
+    end
 
-//outputs
-assign horizontal_sync = (horizontal_count < 96) ? 1'h1 : 1'h0;
-assign vertical_sync = (vertical_count < 2)      ? 1'h1 : 1'h0;
-
-// THIS IS NOT MY CODE!!!!! USESD FOR TESTING PURPROSES
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// pattern generate
-		always @ (posedge clk)
-		begin
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 1
-			if (vertical_count < 135)
-				begin              
-					r_red <= 4'hF;    // white
-					r_blue <= 4'hF;
-					r_green <= 4'hF;
-				end  // if (vertical_count < 135)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 1
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 2
-			else if (vertical_count >= 135 && vertical_count < 205)
-				begin 
-					if (horizontal_count < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (horizontal_count < 324)
-					else if (horizontal_count >= 324 && horizontal_count < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 324 && horizontal_count < 604)
-					else if (horizontal_count >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 604)
-					end  // else if (vertical_count >= 135 && vertical_count < 205)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 2
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 3
-			else if (vertical_count >= 205 && vertical_count < 217)
-				begin 
-					if (horizontal_count < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (horizontal_count < 324)
-					else if (horizontal_count >= 324 && horizontal_count < 371)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 324 && horizontal_count < 371)
-					else if (horizontal_count >= 371 && horizontal_count < 383)
-						begin 
-							r_red <= 4'h0;    // black
-							r_blue <= 4'h0;
-							r_green <= 4'h0;
-						end  // else if (horizontal_count >= 371 && horizontal_count < 383)
-					else if (horizontal_count >= 383 && horizontal_count < 545)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 383 && horizontal_count < 545)
-					else if (horizontal_count >= 545 && horizontal_count < 557)
-						begin 
-							r_red <= 4'h0;    // black
-							r_blue <= 4'h0;
-							r_green <= 4'h0;
-						end  // else if (horizontal_count >= 545 && horizontal_count < 557)
-					else if (horizontal_count >= 557 && horizontal_count < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 557 && horizontal_count < 604)
-					else if (horizontal_count >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 604)
-				end  // else if (vertical_count >= 205 && vertical_count < 217)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 3
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 4
-			else if (vertical_count >= 217 && vertical_count < 305)
-				begin
-					if (horizontal_count < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (horizontal_count < 324)
-					else if (horizontal_count >= 324 && horizontal_count < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 324 && horizontal_count < 604)
-					else if (horizontal_count >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 604)	
-				end  // else if (vertical_count >= 217 && vertical_count < 305)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 4
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 5
-			else if (vertical_count >= 305 && vertical_count < 310)
-				begin
-					if (horizontal_count < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (horizontal_count < 324)
-					else if (horizontal_count >= 324 && horizontal_count < 371)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 324 && horizontal_count < 371)
-					else if (horizontal_count >= 371 && horizontal_count < 557)
-						begin 
-							r_red <= 4'h0;    // black
-							r_blue <= 4'h0;
-							r_green <= 4'h0;
-						end  // else if (horizontal_count >= 371 && horizontal_count < 557)
-					else if (horizontal_count >= 557 && horizontal_count < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 557 && horizontal_count < 604)
-					else if (horizontal_count >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 604)	
-				end  // else if (vertical_count >= 217 && vertical_count < 305)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 5
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 6
-			else if (vertical_count >= 305 && vertical_count < 414)
-				begin
-					if (horizontal_count < 324)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // if (horizontal_count < 324)
-					else if (horizontal_count >= 324 && horizontal_count < 604)
-						begin 
-							r_red <= 4'hF;    // yellow
-							r_blue <= 4'h0;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 324 && horizontal_count < 604)
-					else if (horizontal_count >= 604)
-						begin 
-							r_red <= 4'hF;    // white
-							r_blue <= 4'hF;
-							r_green <= 4'hF;
-						end  // else if (horizontal_count >= 604)	
-				end  // else if (vertical_count >= 305 && vertical_count < 414)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 6
-			
-			////////////////////////////////////////////////////////////////////////////////////// SECTION 7
-			else if (vertical_count <= 414)
-				begin              
-					r_red <= 4'hF;    // white
-					r_blue <= 4'hF;
-					r_green <= 4'hF;
-				end  // if (vertical_count >= 414)
-			////////////////////////////////////////////////////////////////////////////////////// END SECTION 7
-		end  // always
-						
-	// end pattern generate
-
-    always @(horizontal_count, vertical_count) begin
-    if ((horizontal_count >= 96 && horizontal_count <= 144) ||
-         (horizontal_count>= 784 && horizontal_count <= 799) ||
-         (vertical_count >= 2 && vertical_count <= 35) ||
-         (vertical_count >= 515 && vertical_count <= 524))
-         vga_blank_n = 1'b0;
+    assign hsync = ~((hcount >= H_FRONT_PORCH) & (hcount < H_FRONT_PORCH + H_SYNC));
+    assign vsync = ~((vcount >= V_DISPLAY_INT + V_FRONT_PORCH) & (vcount < V_DISPLAY_INT + V_BACK_PORCH + V_SYNC));
+    
+//    assign red = 8'b00101101;
+//    assign green = 8'b10011010;
+//    assign blue = 8'b01111001;
+    
+//    always @(posedge clk) begin
+//        vgaClk = ~vgaClk;
+//    end
+    
+    always @(posedge vgaClk) begin
+        if (hcount < H_TOTAL) begin
+            hcount = hcount + 1;
+            vcount = vcount;
+        end
+        else if (vcount < V_TOTAL) begin
+            hcount = 0;
+            vcount = vcount + 1;
+        end
+        else begin
+            hcount = 0;
+            vcount = 0;
+        end
+    end
+    
+    always @(hcount, vcount) begin
+//				if (mem_in > 239) begin 
+//				 h_range_i = 239 + 100; 
+//				end 
+//				if (mem_in < 239) begin 
+//				 h_range_i = 239 - 100; 
+//				end 
+//				
+//				if (mem_in == 239) begin 
+//				 h_range_i = 239; 
+//				end 
+             h_range_i = mem_in; 
+				 //h_range_i = 450; 
+             h_range_e =  h_range_i + 8'b10100000; 
+            if(vcount > 410 && vcount < 420 && hcount > h_range_i && hcount < h_range_e) begin 
+                red = 8'd255;
+                green = 8'd255;
+                blue = 8'd255;
+                     end 
+                     else begin
+                red = 8'd12;
+                green = 8'd1;
+                blue = 8'd181;
+                end
+        end
+    
+    always @(hcount,vcount) begin
+    
+    // bright
+    if ((hcount >= H_FRONT_PORCH + H_SYNC + H_BACK_PORCH) &&
+         (hcount < H_TOTAL - H_FRONT_PORCH) &&
+         (vcount < V_DISPLAY_INT + V_BACK_PORCH + V_SYNC))
+         blankN = 1'b1;
      
      else 
-        vga_blank_n = 1'b1; // bright
-end
+        blankN = 1'b0;
+        
+    end
+    
+endmodule
 
-// Update RGB Values 
-assign r = (horizontal_count < 784 && horizontal_count > 144 && vertical_count < 515 && vertical_count > 35) ? r_red : 4'h0;
-assign g = (horizontal_count < 784 && horizontal_count > 144 && vertical_count < 515 && vertical_count > 35) ? r_green : 4'h0;
-assign b = (horizontal_count < 784 && horizontal_count > 144 && vertical_count < 515 && vertical_count > 35) ? r_blue: 4'h0;
-
-
-endmodule //vga_control
+module vgaRegister(in, clk, en, out);
+    input [15:0] in;
+    input clk, en;
+    
+    output reg [15:0] out;
+    
+    always @(negedge clk) begin
+        if (en)
+            out <= in;
+        else
+            out <= out;
+    end
+endmodule
